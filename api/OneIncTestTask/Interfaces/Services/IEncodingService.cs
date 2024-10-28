dee9fac9f3be347cde2e2e0a4e8c6367c06a19b9
Elias Alvarez <dev.eliasalvarez@gmaill.com>
2024-10-28 11:52:54 -0300

    Add encoding service with get encoding input text endpoint.

---
+++ api/OneIncTestTask/Interfaces/Services/IEncodingService.cs
@@ -0,0 +1,4 @@
+public interface IEncodingService 
+{
+    public Task<string> GetEncodingInputText(string inputText);
+}
---
+++ api/OneIncTestTask/Services/EncodingService.cs
@@ -0,0 +1,10 @@
+namespace OneIncTestTask.Api.Services 
+{
+    public class EncodingService: IEncodingService 
+    {
+        public async Task<string> GetEncodingInputText(string inputText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
