08561807cb1e94b5f7df9619d3d2621ab1aa7576
Elias Alvarez <dev.eliasalvarez@gmaill.com>
2024-10-28 23:12:11 -0300

    Add styles and text encoder component

--- web/OneIncTestTask/src/app/pages/home/home.component.ts
+++ web/OneIncTestTask/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TextEncoderComponent } from "../../shared/text-encoder/text-encoder.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TextEncoderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ web/OneIncTestTask/src/app/shared/text-encoder/text-encoder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-encoder',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-encoder.component.html',
+  styleUrl: './text-encoder.component.css'
+})
+export class TextEncoderComponent {
+
+}
